a3669f9f7a6a20f973cf428273dd729f1127bb74
wanpergit <nxb2017@163.com>
2021-01-08 23:50:57 +0800

    first commit

---
+++ src/test/java/com/mvp/MvpApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
